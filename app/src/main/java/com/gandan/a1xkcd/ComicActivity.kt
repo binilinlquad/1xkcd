3de996fc147f47aa59607fa546ce512b907c1950
Chandra Yang <otama.ariq@gmail.com>
2019-02-19 06:45:31 +0700

    Cancel coroutine with job

--- app/src/main/java/com/gandan/a1xkcd/ComicActivity.kt
+++ app/src/main/java/com/gandan/a1xkcd/ComicActivity.kt
@@ -1,7 +1,6 @@
 package com.gandan.a1xkcd
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.paging.LivePagedListBuilder
@@ -11,16 +10,16 @@ import com.gandan.a1xkcd.comic.ui.ComicPageAdapter
 import com.gandan.a1xkcd.comic.ui.PageDataSourceFactory
 import com.gandan.a1xkcd.service.Page
 import com.gandan.a1xkcd.service.XkcdService
-import com.gandan.a1xkcd.service.createXkcdService
-import dagger.android.AndroidInjection
-import dagger.android.support.AndroidSupportInjection
 import dagger.android.support.DaggerAppCompatActivity
 import kotlinx.android.synthetic.main.activity_comics.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancelChildren
 import javax.inject.Inject
 
 class ComicActivity : DaggerAppCompatActivity() {
+    private val job = Job() + Dispatchers.Main
     @Inject
     lateinit var service: XkcdService
 
@@ -28,9 +27,7 @@ class ComicActivity : DaggerAppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_comics)
 
-        // basic http client
-//        val service = createXkcdService("https://xkcd.com/")
-        val uiScope = CoroutineScope(Dispatchers.Main)
+        val uiScope = CoroutineScope(job)
 
         val pageSourceFactory = PageDataSourceFactory(service, uiScope, this)
 
@@ -41,5 +38,10 @@ class ComicActivity : DaggerAppCompatActivity() {
         comics.adapter = pagedPageAdapter
 
         livePages.observe(this, Observer { pagedList -> pagedPageAdapter.submitList(pagedList) })
+    }
+
+    override fun onDestroy() {
+        job.cancelChildren()
+        super.onDestroy()
     }
 }
