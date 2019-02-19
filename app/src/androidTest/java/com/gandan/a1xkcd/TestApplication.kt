6052207ca1dacd07d12dbe39fc61e4df670d1b74
Chandra Yang <otama.ariq@gmail.com>
2019-02-19 22:51:05 +0700

    Remove permitNetwork in main thread

--- app/src/androidTest/java/com/gandan/a1xkcd/TestApplication.kt
+++ app/src/androidTest/java/com/gandan/a1xkcd/TestApplication.kt
@@ -1,19 +1,9 @@
 package com.gandan.a1xkcd
 
-import android.os.StrictMode
 import com.gandan.a1xkcd.di.AppComponent
 import com.gandan.a1xkcd.di.DaggerTestAppComponent
 
 class TestApplication : ComicApplication() {
-    override fun onCreate() {
-        super.onCreate()
-        StrictMode.setThreadPolicy(
-            StrictMode.ThreadPolicy.Builder()
-                .permitNetwork()
-                .build()
-        )
-    }
-
     override fun applicationInjector(): AppComponent {
         // follow sample https://google.github.io/dagger/testing.html
         return DaggerTestAppComponent.create()
