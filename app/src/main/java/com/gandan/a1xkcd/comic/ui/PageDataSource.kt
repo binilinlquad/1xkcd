c8af2b3c46539f6645eb44842c245dcae459ce82
Chandra Yang <otama.ariq@gmail.com>
2019-05-19 23:59:49 +0700

    Rough implementation use async and Dispatcher.IO for network call
    avoid blocking UI Scope when network call

--- app/src/main/java/com/gandan/a1xkcd/comic/ui/PageDataSource.kt
+++ app/src/main/java/com/gandan/a1xkcd/comic/ui/PageDataSource.kt
@@ -12,12 +12,14 @@ import com.gandan.a1xkcd.service.Page
 import com.gandan.a1xkcd.service.XkcdService
 import kotlinx.android.synthetic.main.activity_comics.*
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 
 class PageDataSourceFactory(
-        private val service: XkcdService,
-        private val uiScope: CoroutineScope,
-        private val activity: ComicActivity
+    private val service: XkcdService,
+    private val uiScope: CoroutineScope,
+    private val activity: ComicActivity
 ) : DataSource.Factory<Int, Page>() {
     private val sourceLiveData = MutableLiveData<PageDataSource>()
     override fun create(): DataSource<Int, Page> {
@@ -28,9 +30,11 @@ class PageDataSourceFactory(
 
 }
 
-class PageDataSource(private val service: XkcdService,
-                     private val uiScope: CoroutineScope,
-                     private val activity: ComicActivity) : ItemKeyedDataSource<Int, Page>() {
+class PageDataSource(
+    private val service: XkcdService,
+    private val uiScope: CoroutineScope,
+    private val activity: ComicActivity
+) : ItemKeyedDataSource<Int, Page>() {
 
     companion object {
         private val TAG = PageDataSource::class.java.simpleName
@@ -38,22 +42,30 @@ class PageDataSource(private val service: XkcdService,
 
     override fun loadInitial(params: LoadInitialParams<Int>, callback: LoadInitialCallback<Page>) {
         uiScope.launch {
-            try {
-                val latestPage = service.latestStrip().await()
-                val totalPages = latestPage.num
-                callback.onResult(listOf(latestPage), 0, totalPages)
 
-                Log.i(TAG, "load latest page successfully")
+            async(Dispatchers.IO) {
+                try {
+                    val latestPage = service.latestStrip().await()
+                    val totalPages = latestPage.num
 
-                activity.manual_refresh.visibility = View.GONE
-                activity.comics.visibility = View.VISIBLE
+                    uiScope.launch {
+                        callback.onResult(listOf(latestPage), 0, totalPages)
 
-            } catch (e: Throwable) {
-                Log.i(TAG, "fail loading latest page with reason ${e.message}")
-                Toast.makeText(activity, e.message, Toast.LENGTH_LONG).show()
+                        Log.i(TAG, "load latest page successfully")
 
-                activity.manual_refresh.visibility = View.VISIBLE
-                activity.comics.visibility = View.GONE
+                        activity.manual_refresh.visibility = View.GONE
+                        activity.comics.visibility = View.VISIBLE
+                    }
+                } catch (e: Throwable) {
+                    uiScope.launch {
+                        Log.i(TAG, "fail loading latest page with reason ${e.message}")
+                        Toast.makeText(activity, e.message, Toast.LENGTH_LONG).show()
+
+                        activity.manual_refresh.visibility = View.VISIBLE
+                        activity.comics.visibility = View.GONE
+                    }
+                }
+
             }
         }
     }
