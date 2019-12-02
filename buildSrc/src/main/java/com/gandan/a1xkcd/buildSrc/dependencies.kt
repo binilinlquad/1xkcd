8aa808e422d1f6769cbe19432e4181e70675b598
Chandra Yang <otama.ariq@gmail.com>
2019-12-02 23:57:24 +0700

    extract androidX dependencies using gradle kotlin dialect

---
+++ buildSrc/src/main/java/com/gandan/a1xkcd/buildSrc/dependencies.kt
@@ -0,0 +1,10 @@
+package com.gandan.a1xkcd.buildSrc
+
+object AndroidX {
+    const val appCompat = "androidx.appcompat:appcompat:1.1.0"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:1.1.3"
+    const val recyclerView = "androidx.recyclerview:recyclerview:1.0.0"
+    const val cardView = "androidx.cardview:cardview:1.0.0"
+    const val lifecyleExtension = "android.arch.lifecycle:extensions:1.1.1"
+    const val pagingRuntime = "androidx.paging:paging-runtime-ktx:2.1.0"
+}
