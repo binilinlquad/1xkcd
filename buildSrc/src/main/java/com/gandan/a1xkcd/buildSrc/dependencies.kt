bce6364a500e3ddb254f148382aef7bf3347217a
Chandra Yang <otama.ariq@gmail.com>
2019-12-06 08:48:42 +0700

    upgrade constraint-layout to 2.0.0-beta3

--- buildSrc/src/main/java/com/gandan/a1xkcd/buildSrc/dependencies.kt
+++ buildSrc/src/main/java/com/gandan/a1xkcd/buildSrc/dependencies.kt
@@ -3,7 +3,7 @@ package com.gandan.a1xkcd.buildSrc
 object Libs {
     object AndroidX {
         const val appCompat = "androidx.appcompat:appcompat:1.1.0"
-        const val constraintLayout = "androidx.constraintlayout:constraintlayout:1.1.3"
+        const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.0.0-beta3"
         const val recyclerView = "androidx.recyclerview:recyclerview:1.0.0"
         const val cardView = "androidx.cardview:cardview:1.0.0"
         const val lifecyleExtension = "android.arch.lifecycle:extensions:1.1.1"
