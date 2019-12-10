b1d910832b98c45aabb7da8d7f72e34830ae0403
Chandra Yang <otama.ariq@gmail.com>
2019-12-11 01:06:09 +0700

    upgrade recyclerview version to 1.1.0
    and adding swiperefreshlayout library. It may transitively included
    when using recyclerview version 1.0.01. Without it, application cannot
    be compiled.

--- buildSrc/src/main/java/com/gandan/a1xkcd/buildSrc/dependencies.kt
+++ buildSrc/src/main/java/com/gandan/a1xkcd/buildSrc/dependencies.kt
@@ -4,10 +4,11 @@ object Libs {
     object AndroidX {
         const val appCompat = "androidx.appcompat:appcompat:1.1.0"
         const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.0.0-beta3"
-        const val recyclerView = "androidx.recyclerview:recyclerview:1.0.0"
+        const val recyclerView = "androidx.recyclerview:recyclerview:1.1.0"
         const val cardView = "androidx.cardview:cardview:1.0.0"
         const val lifecyleExtension = "android.arch.lifecycle:extensions:1.1.1"
         const val pagingRuntime = "androidx.paging:paging-runtime-ktx:2.1.0"
+        const val swipeRefreshLayout = "androidx.swiperefreshlayout:swiperefreshlayout:1.0.0"
 
         object Test {
             private const val version = "1.1.1"
