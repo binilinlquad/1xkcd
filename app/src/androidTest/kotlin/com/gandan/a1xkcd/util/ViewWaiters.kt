96f979f8bd0e808cd95149a8b4736d55d05e1a20
Chandra Yang <otama.ariq@gmail.com>
2020-02-02 18:57:41 +0700

    Fix view waiters
    - waiting until not displayed if view has been not displayed at first
    - add action to wait until view is displayed
    
    Signed-off-by: Chandra Yang <otama.ariq@gmail.com>

--- app/src/androidTest/kotlin/com/gandan/a1xkcd/util/ViewWaiters.kt
+++ app/src/androidTest/kotlin/com/gandan/a1xkcd/util/ViewWaiters.kt
@@ -9,11 +9,8 @@ import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.util.HumanReadables
 import org.hamcrest.Matcher
 
-fun waitUntilNotDisplayed(): ViewAction {
-    return NotDisplayedWaiter()
-}
+fun waitUntilNotDisplayed() = NotDisplayedWaiter()
 
-// TODO : will fail when view has been not displayed when action is performed
 class NotDisplayedWaiter : ViewAction {
     private companion object {
         private const val timeout = 30_000L
@@ -21,9 +18,17 @@ class NotDisplayedWaiter : ViewAction {
 
     override fun getDescription(): String = "Wait until view is not displayed"
 
-    override fun getConstraints(): Matcher<View> = ViewMatchers.isDisplayed()
+    override fun getConstraints(): Matcher<View> = ViewMatchers.isAssignableFrom(View::class.java)
 
     override fun perform(uiController: UiController?, view: View?) {
+        // if view is not displayed
+        if (!ViewMatchers.isDisplayed().matches(view)) {
+            uiController?.loopMainThreadUntilIdle()
+
+            // recheck that view is not displayed and do nothing
+            if (!ViewMatchers.isDisplayed().matches(view)) return
+        }
+
         val start = SystemClock.currentThreadTimeMillis()
         do {
             uiController?.loopMainThreadUntilIdle()
@@ -36,5 +41,40 @@ class NotDisplayedWaiter : ViewAction {
                     .build()
             }
         } while (ViewMatchers.isDisplayed().matches(view))
+    }
+}
+
+fun waitUntilDisplayed(): ViewAction = DisplayedWaiter()
+
+class DisplayedWaiter : ViewAction {
+    private companion object {
+        private const val timeout = 30_000L
+    }
+
+    override fun getDescription(): String = "Wait until view is displayed"
+
+    override fun getConstraints(): Matcher<View> = ViewMatchers.isAssignableFrom(View::class.java)
+
+    override fun perform(uiController: UiController?, view: View?) {
+        // if view is not displayed
+        if (ViewMatchers.isDisplayed().matches(view)) {
+            uiController?.loopMainThreadUntilIdle()
+
+            // recheck that view is displayed and do nothing
+            if (ViewMatchers.isDisplayed().matches(view)) return
+        }
+
+        val start = SystemClock.currentThreadTimeMillis()
+        do {
+            uiController?.loopMainThreadUntilIdle()
+            val now = SystemClock.currentThreadTimeMillis()
+            if (timeout < now - start) {
+                throw PerformException.Builder()
+                    .withActionDescription(this.getDescription())
+                    .withViewDescription(HumanReadables.describe(view))
+                    .withCause(RuntimeException("View is never displayed after wait for $timeout ms"))
+                    .build()
+            }
+        } while (!ViewMatchers.isDisplayed().matches(view))
     }
 }
