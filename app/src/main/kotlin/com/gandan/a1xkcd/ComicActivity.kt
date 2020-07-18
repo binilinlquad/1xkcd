32e433afc829b682d1061e4dfa654945560a24fe
chandra <otama.ariq@gmail.com>
2020-07-18 17:49:27 +0700

    Extract View operation to it's own class
    Stop putting View operationin Activity. Let's make Activity as entry point and context provider only.

--- app/src/main/kotlin/com/gandan/a1xkcd/ComicActivity.kt
+++ app/src/main/kotlin/com/gandan/a1xkcd/ComicActivity.kt
@@ -4,23 +4,16 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import androidx.recyclerview.widget.LinearLayoutManager
 import coil.Coil
 import coil.ImageLoaderBuilder
-import com.gandan.a1xkcd.comic.model.MainState
-import com.gandan.a1xkcd.comic.ui.ComicPageAdapter
+import com.gandan.a1xkcd.comic.ui.MainView
 import com.gandan.a1xkcd.comic.viewModel.MainViewModel
 import com.gandan.a1xkcd.service.XkcdService
 import com.gandan.a1xkcd.ui.GoToButtonHandler
-import com.gandan.a1xkcd.ui.PageGoToButtonHandler
 import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.activity_comics.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.cancel
@@ -28,7 +21,7 @@ import okhttp3.OkHttpClient
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class ComicActivity :AppCompatActivity(), CoroutineScope by MainScope() {
+class ComicActivity : AppCompatActivity(), CoroutineScope by MainScope() {
 
     @Inject
     lateinit var okHttp3Client: OkHttpClient
@@ -39,20 +32,16 @@ class ComicActivity :AppCompatActivity(), CoroutineScope by MainScope() {
     private lateinit var goToButtonHandler: GoToButtonHandler
 
     private lateinit var mainViewModel: MainViewModel
+    private lateinit var mainView: MainView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initializeCoil()
         setContentView(R.layout.activity_comics)
         mainViewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-    }
+        mainView = MainView(this, this.findViewById(android.R.id.content), service)
+        mainView.bind(mainViewModel)
 
-    override fun onPostCreate(savedInstanceState: Bundle?) {
-        super.onPostCreate(savedInstanceState)
-        comics_refresher.setOnRefreshListener { resetPagesAndRefresh() }
-        manual_refresh.setOnClickListener { resetPagesAndRefresh() }
-
-        resetPagesAndRefresh()
     }
 
     private fun initializeCoil() {
@@ -63,51 +52,7 @@ class ComicActivity :AppCompatActivity(), CoroutineScope by MainScope() {
         )
     }
 
-    private fun renderEmpty() {
-        showEmptyPlaceholder()
-        disableGoToButton()
-    }
-
-    private fun showEmptyPlaceholder() {
-        comics_refresher.isRefreshing = false
-        manual_refresh.visibility = View.VISIBLE
-        comics.visibility = View.GONE
-    }
-
-    private fun renderPages(state: MainState.ShowComic) {
-        showComicPages()
-        enableGoToButton(state)
-    }
-
-    private fun enableGoToButton(state: MainState.ShowComic) {
-        goToButtonHandler = PageGoToButtonHandler(this, state.totalPages, comics::scrollToPosition)
-        gotoMenu?.isEnabled = true
-    }
-
-    private fun showComicPages() {
-        comics_refresher.isRefreshing = false
-        manual_refresh.visibility = View.GONE
-        comics.visibility = View.VISIBLE
-    }
-
-    private fun renderRefresh() {
-        comics_refresher.isRefreshing = true
-    }
-
-    private fun renderError(state: MainState.Error) {
-        showError(state)
-        disableGoToButton()
-    }
-
-    private fun showError(state: MainState.Error) {
-        Toast.makeText(this, state.error.message, Toast.LENGTH_LONG).show()
-    }
-
-    private fun disableGoToButton() {
-        gotoMenu?.isEnabled = false
-    }
-
-    override fun onDestroy() {
+   override fun onDestroy() {
         cancel()
         super.onDestroy()
     }
@@ -120,20 +65,17 @@ class ComicActivity :AppCompatActivity(), CoroutineScope by MainScope() {
     private var gotoMenu: MenuItem? = null
     override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
         gotoMenu = menu?.findItem(R.id.menu_goto)
-        gotoMenu?.run {
-            mainViewModel.state.value?.let { render(it) }
-        }
         return super.onPrepareOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.menu_refresh -> {
-                resetPagesAndRefresh()
+                mainView.resetPagesAndRefresh()
                 true
             }
             R.id.menu_goto -> {
-                goToButtonHandler.onClick()
+                mainView.clickGoTo()
                 true
             }
             R.id.menu_open_source_licenses -> {
@@ -142,37 +84,6 @@ class ComicActivity :AppCompatActivity(), CoroutineScope by MainScope() {
             }
             else -> super.onOptionsItemSelected(item)
         }
-    }
-
-    private fun render(state: MainState) {
-        when (state) {
-            is MainState.Empty -> {
-                renderEmpty()
-            }
-            is MainState.ShowComic -> {
-                renderPages(state)
-            }
-            is MainState.Error -> {
-                renderError(state)
-            }
-            is MainState.Refresh -> {
-                renderRefresh()
-            }
-        }
-    }
-
-    private fun resetPagesAndRefresh() {
-        mainViewModel.state.observe(this, Observer { event -> render(event) })
-        mainViewModel.bind(service)
-
-        comics.layoutManager = LinearLayoutManager(this)
-        val pagedPageAdapter = ComicPageAdapter()
-        comics.swapAdapter(pagedPageAdapter, false)
-
-        // still looking how to put it nicely with viewmodel
-        mainViewModel.pages.observe(this, Observer { pagedList ->
-            pagedPageAdapter.submitList(pagedList)
-        })
     }
 
 }
---
+++ app/src/main/kotlin/com/gandan/a1xkcd/comic/ui/MainView.kt
@@ -0,0 +1,139 @@
+package com.gandan.a1xkcd.comic.ui
+
+import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.gandan.a1xkcd.R
+import com.gandan.a1xkcd.comic.model.MainState
+import com.gandan.a1xkcd.comic.viewModel.MainViewModel
+import com.gandan.a1xkcd.service.XkcdService
+import com.gandan.a1xkcd.ui.GoToButtonHandler
+import com.gandan.a1xkcd.ui.PageGoToButtonHandler
+
+@Suppress("PrivatePropertyName")
+class MainView(
+    private val owner: LifecycleOwner,
+    private val root: View,
+    private val service: XkcdService
+) {
+
+    private lateinit var comics_refresher: SwipeRefreshLayout
+    private lateinit var  manual_refresh: TextView
+    private lateinit var  comics: RecyclerView
+    private lateinit var mainViewModel: MainViewModel
+    private lateinit var goToButtonHandler: GoToButtonHandler
+
+    var gotoMenu: MenuItem? = null
+    set(value) {
+        field = value
+        if (field != null) {
+            mainViewModel.state.value?.let { render(it) }
+        }
+    }
+
+    init {
+        root.post {
+            comics_refresher  = root.findViewById(R.id.comics_refresher)
+            manual_refresh = root.findViewById(R.id.manual_refresh)
+            comics = root.findViewById(R.id.comics)
+        }
+    }
+
+    fun bind(viewModel: MainViewModel) {
+        mainViewModel = viewModel
+        root.post {
+            comics_refresher.setOnRefreshListener { resetPagesAndRefresh() }
+            manual_refresh.setOnClickListener { resetPagesAndRefresh() }
+
+            resetPagesAndRefresh()
+        }
+    }
+
+    private fun render(state: MainState) {
+        root.post {
+            when (state) {
+                is MainState.Empty -> {
+                    renderEmpty()
+                }
+                is MainState.ShowComic -> {
+                    renderPages(state)
+                }
+                is MainState.Error -> {
+                    renderError(state)
+                }
+                is MainState.Refresh -> {
+                    renderRefresh()
+                }
+            }
+        }
+    }
+
+    private fun renderEmpty() {
+        showEmptyPlaceholder()
+        disableGoToButton()
+    }
+
+    private fun renderPages(state: MainState.ShowComic) {
+        showComicPages()
+        enableGoToButton(state)
+    }
+
+    private fun renderError(state: MainState.Error) {
+        showError(state)
+        disableGoToButton()
+    }
+
+    private fun renderRefresh() {
+        comics_refresher.isRefreshing = true
+    }
+
+    fun resetPagesAndRefresh() {
+        mainViewModel.state.observe(owner, Observer { event -> render(event) })
+        mainViewModel.bind(service)
+
+        comics.layoutManager = LinearLayoutManager(root.context)
+        val pagedPageAdapter = ComicPageAdapter()
+        comics.swapAdapter(pagedPageAdapter, false)
+
+        // still looking how to put it nicely with viewmodel
+        mainViewModel.pages.observe(owner, Observer { pagedList ->
+            pagedPageAdapter.submitList(pagedList)
+        })
+    }
+
+    private fun showEmptyPlaceholder() {
+        comics_refresher.isRefreshing = false
+        manual_refresh.visibility = View.VISIBLE
+        comics.visibility = View.GONE
+    }
+
+    private fun disableGoToButton() {
+        gotoMenu?.isEnabled = false
+    }
+
+    private fun enableGoToButton(state: MainState.ShowComic) {
+        goToButtonHandler = PageGoToButtonHandler(root.context, state.totalPages, comics::scrollToPosition)
+        gotoMenu?.isEnabled = true
+    }
+
+    private fun showComicPages() {
+        comics_refresher.isRefreshing = false
+        manual_refresh.visibility = View.GONE
+        comics.visibility = View.VISIBLE
+    }
+
+    private fun showError(state: MainState.Error) {
+        Toast.makeText(root.context, state.error.message, Toast.LENGTH_LONG).show()
+    }
+
+    fun clickGoTo() {
+        goToButtonHandler.onClick()
+    }
+
+}
