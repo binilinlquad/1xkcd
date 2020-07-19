0089b06ab1ef6cc11c7fb656cad7cb4db599d5b4
chandra <otama.ariq@gmail.com>
2020-07-19 15:40:06 +0700

    Set correct style for GoTo dialog
    After separate view operation to other class, GoTo shows dialog with dark background behind black text, make it hard for reader to select page number.
    
    The reason behind this behavior because PageGoToButtonHandler use view context (before Activity context) and inherit context style. Fix it by passing intended style.
    
    Android has scarce documentation about style. Passed style is tested with Android emulator version 30, 23, and 21. And real device Samsung Galaxy S10 with android 29.

--- app/src/main/kotlin/com/gandan/a1xkcd/ui/PageGoToButtonHandler.kt
+++ app/src/main/kotlin/com/gandan/a1xkcd/ui/PageGoToButtonHandler.kt
@@ -2,6 +2,7 @@ package com.gandan.a1xkcd.ui
 
 import android.content.Context
 import android.content.DialogInterface
+import android.os.Build
 import androidx.appcompat.app.AlertDialog
 
 class PageGoToButtonHandler(
@@ -10,6 +11,11 @@ class PageGoToButtonHandler(
     private val scroller: (Int) -> Unit
 ) : GoToButtonHandler {
     private var pages: Array<String>
+    private val style = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        android.R.style.ThemeOverlay_Material_Dialog_Alert
+    } else {
+        android.R.style.Theme_Material_Light_Dialog_NoActionBar
+    }
 
     init {
         if (totalPages < 1) throw IllegalArgumentException("Total page should bigger than 0 but $totalPages")
@@ -18,7 +24,7 @@ class PageGoToButtonHandler(
     }
 
     override fun onClick() {
-        AlertDialog.Builder(context)
+        AlertDialog.Builder(context, style)
             .setItems(pages) { dialog: DialogInterface, whichPage: Int ->
                 scroller(whichPage)
                 dialog.dismiss()
