4c04d9b5b89da402f37b361fb6742b942e5cb504
chandra <otama.ariq@gmail.com>
2021-08-29 19:19:06 +0700

    Annotate ComicStrip with ExperimentalSerializationApi

--- app/src/main/java/com/gandan/c1xkcd/ui/screen/ComicStrip.kt
+++ app/src/main/java/com/gandan/c1xkcd/ui/screen/ComicStrip.kt
@@ -9,16 +9,16 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.gandan.c1xkcd.MainViewModel
+import kotlinx.serialization.ExperimentalSerializationApi
 
+@ExperimentalSerializationApi
 @Composable
 fun ComicStrip(viewModel: MainViewModel) {
-
     val state = viewModel.latest.observeAsState()
     val strip by remember { state }
 
     val errorState = viewModel.error.observeAsState()
     val error by remember { errorState }
-
 
     if (error == true) {
         Title("Error happened.")
