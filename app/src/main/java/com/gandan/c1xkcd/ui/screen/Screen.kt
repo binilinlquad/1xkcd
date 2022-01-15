fcbba7b7d3d63faaeb698773d8465ab4bb5b0e32
chandra <otama.ariq@gmail.com>
2022-01-15 17:41:05 +0700

    Reformat

--- app/src/main/java/com/gandan/c1xkcd/ui/screen/Screen.kt
+++ app/src/main/java/com/gandan/c1xkcd/ui/screen/Screen.kt
@@ -44,9 +44,9 @@ fun InfiniteCircularProgressAnimation() {
 fun infiniteProgressAnimation(): State<Float> {
     val progressState = rememberInfiniteTransition()
     return progressState.animateFloat(
-        initialValue = 0f, targetValue = 1f, animationSpec = infiniteRepeatable(
-            animation = tween(durationMillis = 750)
-        )
+        initialValue = 0f,
+        targetValue = 1f,
+        animationSpec = infiniteRepeatable(animation = tween(durationMillis = 750))
     )
 }
 
@@ -62,12 +62,12 @@ fun TopAppBar() {
 fun DefaultPreview() {
     val error: State<Throwable?> = mutableStateOf(null)
     val comicStrip: State<Strip?> = mutableStateOf(
-            Strip(
-                1,
-                "Look at this Awesome comic!!!",
-                "https://picsum.photos/200/300",
-                "It is so awesome!"
-            )
+        Strip(
+            1,
+            "Look at this Awesome comic!!!",
+            "https://picsum.photos/200/300",
+            "It is so awesome!"
+        )
     )
 
     C1XkcdTheme {
